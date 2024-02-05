4d6f777f724c1f6e2ffdf415affaf0621ece323a
yeonjikimro <duswlskfk42@naver.com>
2024-02-05 23:46:11 +0900

    스프링부터 서블릿

---
+++ springboot/server/src/main/java/hello/container/AppInit.java
@@ -0,0 +1,10 @@
+package hello.container;
+
+import jakarta.servlet.ServletContext;
+
+public interface AppInit {
+
+    void onStartup(ServletContext servletContext);
+
+
+}
---
+++ springboot/server/src/main/java/hello/container/MyContainerInitV2.java
@@ -0,0 +1,19 @@
+package hello.container;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HandlesTypes;
+
+import java.util.Set;
+
+@HandlesTypes(AppInit.class)
+public class MyContainerInitV2 implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+
+        System.out.println("MyContainerInitV2.onStartup");
+        System.out.println("MyContainerInitV2 c = " + c);
+        System.out.println("MyContainerInitV2 ctx = " + ctx);
+    }
+}
